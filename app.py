c8e142f28e6b00e5cb4ac47c9d55dd81c9b45daa
Bastien Roques <bastien.roques971@gmail.com>
2021-06-02 11:21:01 +0200

    add artists

--- app.py
+++ app.py
@@ -241,17 +241,16 @@ def delete_venue(venue_id):
 #  ----------------------------------------------------------------
 @app.route('/artists')
 def artists():
-    # TODO: replace with real data returned from querying the database
-    data = [{
-        "id": 4,
-        "name": "Guns N Petals",
-    }, {
-        "id": 5,
-        "name": "Matt Quevedo",
-    }, {
-        "id": 6,
-        "name": "The Wild Sax Band",
-    }]
+    data = []
+    # get all artists
+    artists = Artist.query.all()
+
+    for artist in artists:
+        data.append({
+            "id": artist.id,
+            "name": artist.name
+        })
+
     return render_template('pages/artists.html', artists=data)
 
 
